1ce3122ccc1ef3288caa12b34a2ecf50741e9c3a
bxf <2874276879@qq.com>
2023-03-09 15:43:31 +0800

    first commit

---
+++ src/main/java/com/baixuefeng/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.baixuefeng.git;
+
+/**
+ * Name:BXF
+ * Description:
+ * Date:2023/3/9
+ **/
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
